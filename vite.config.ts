6b43b87cf23a7e5f2d9b2e56e1953b44e5ad9b4e
garnalam <tranthanhlam2209@gmail.com>
2025-10-28 01:50:16 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.tsx',
-                'resources/js/routes/admin/index.ts'
+                'resources/js/app.tsx'
             ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
